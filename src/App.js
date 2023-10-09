35a18bf90be7c1220e856ed6e59dc28ac53f70ca
Vikram <vikraamnc@gmail.com>
2023-10-10 10:56:23 +1100

    skills

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
-import Info from "./components/Info";
+// import Info from "./components/Info";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
 import Works from "./components/Works";
@@ -17,19 +17,19 @@ function App() {
           <i className="fa-solid fa-circle-half-stroke"></i>
         </label>
         <Routes>
-          <Route path="" element={<Hero />} />
+          <Route path="/navbar" element={<Navbar />} />
           <Route path="/skills" element={<Skills />} />
           <Route path="/works" element={<Works />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
         <Navbar />
-        <Hero />
+        {/* <Hero />*
         <Info />
         <Skills />
         <Works />
         <About />
-        <Contact />
+        <Contact /> */}
         <Footer />
       </div>
     </>
