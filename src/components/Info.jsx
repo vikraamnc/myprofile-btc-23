b526283fe24c49514c9c24d23bb31bd80b9e5c36
Vikram <vikraamnc@gmail.com>
2023-10-10 11:48:10 +1100

    sam

---
+++ src/components/Info.jsx
@@ -0,0 +1,30 @@
+const Skills = () => {
+  return (
+    <section id="skills" className="flex container skills">
+      <h2 className="title">My Skills </h2>
+
+      <div>
+        <div>
+          <i className="fa-brands fa-html5"></i>
+          <span>HTML</span>
+        </div>
+
+        <div>
+          <i className="fa-brands fa-css3-alt"></i>
+          <span>CSS</span>
+        </div>
+
+        <div>
+          <i className="fa-brands fa-js"></i>
+          <span>JavaScript</span>
+        </div>
+
+        <div>
+          <i className="fa-brands fa-react"></i>
+          <span>React</span>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Skills;
